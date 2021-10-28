d1ec0c58c29384e0f44f1b0c0ccbaa6b12d3f384
Larissa Novais <larinovais54@gmail.com>
2021-10-28 15:53:38 -0300

    metodo correto para atualizar carro

--- src/main/java/br/com/zup/ZupCar/controller/CarroController.java
+++ src/main/java/br/com/zup/ZupCar/controller/CarroController.java
@@ -16,7 +16,7 @@ public class CarroController {
     private List<CarroDTO> concessionaria = new ArrayList<>();
 
     @GetMapping //pega a requisição
-    public  List<CarroDTO> exibirListaDeCarros() {
+    public List<CarroDTO> exibirListaDeCarros() {
         return concessionaria;
     }
 
@@ -24,30 +24,35 @@ public class CarroController {
     //response = retorna a requisição/responde
 
     @PostMapping //faz uma requisição
-    @ResponseStatus(HttpStatus.CREATED)
-    public void cadastrarCarro(@RequestBody CarroDTO carroDTO){
+    @ResponseStatus(HttpStatus.CREATED) //mudar status do protocolo http
+    public void cadastrarCarro(@RequestBody CarroDTO carroDTO) {
         concessionaria.add(carroDTO);
     }
 
     @GetMapping("/{nomeDoCarro}")
     public CarroDTO exibirCarro(@PathVariable String nomeDoCarro) {
-        for (CarroDTO referencia: concessionaria) {
-            if (referencia.getModelo().equalsIgnoreCase(nomeDoCarro)){
+        for (CarroDTO referencia : concessionaria) {
+            if (referencia.getModelo().equalsIgnoreCase(nomeDoCarro)) {
                 return referencia;
             }
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não Encontrado");
     }
 
-    @PutMapping("/{novoNome}")
-    public  CarroDTO atualizarCarro(@PathVariable String novoNome){
-        for (CarroDTO referencia: concessionaria) {
-            if (referencia.getModelo().equalsIgnoreCase(novoNome)){
+    @PutMapping("/{novoNome}") //atualizar nome do carro
+    public CarroDTO atualizarCarro(@PathVariable String novoNome, @RequestBody CarroDTO carroDTO) {
+        //for - para pecorrer a lista
+        //set - para atualizar os dados do carro
+        for (CarroDTO referencia : concessionaria) {
+            if (referencia.getModelo().equalsIgnoreCase(novoNome)) {
+                referencia.setCor(carroDTO.getCor());
+                referencia.setMotor(carroDTO.getMotor());
+                referencia.setAno(carroDTO.getAno());
+
                 return referencia;
             }
         }
-        throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não Encontrado");
     }
-
 }
 
